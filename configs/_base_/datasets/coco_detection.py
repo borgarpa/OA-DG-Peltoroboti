15e118be3cd3628cebf8a534233d08a31d06029f
Borja Garcia <64423569+borgarpa@users.noreply.github.com>
2024-04-24 14:46:05 +0300

    Update coco_detection.py

--- configs/_base_/datasets/coco_detection.py
+++ configs/_base_/datasets/coco_detection.py
@@ -2,7 +2,7 @@
 dataset_type = 'CocoDataset'
 data_root = './data/coco/dataset-v3'
 img_norm_cfg = dict(
-    mean=[131.822, 128.974, 120.001], std=[33.116, 31.281, 32.664], to_rgb=True)
+    mean=[131.868, 128.973, 120.391], std=[33.107, 31.271, 32.656], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
