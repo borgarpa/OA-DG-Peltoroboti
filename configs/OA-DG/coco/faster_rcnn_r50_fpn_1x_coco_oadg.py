d35ab50dbd71a1e460d0dad5bc1269f1a2f23a6e
Borja Garcia <64423569+borgarpa@users.noreply.github.com>
2024-05-03 09:04:54 +0300

    Update faster_rcnn_r50_fpn_1x_coco_oadg.py

--- configs/OA-DG/coco/faster_rcnn_r50_fpn_1x_coco_oadg.py
+++ configs/OA-DG/coco/faster_rcnn_r50_fpn_1x_coco_oadg.py
@@ -64,7 +64,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=[(1333, 800), (1333, 1024)], keep_ratio=True),
+    dict(type='Resize', img_scale=[(800, 800), (1024, 1024)], keep_ratio=True), # (1333, 800), (1333, 1024)
     dict(type='RandomFlip', flip_ratio=0.5),
     oamix_config,
     dict(type='Normalize', **img_norm_cfg),
